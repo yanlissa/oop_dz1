24152d0a23f045b929916a695264cc0f3ee83684
Yanovich Liza <yanovich.liza@yandex.ru>
2020-06-18 13:56:54 +0300

    BitString сдвиги

--- BitString.h
+++ BitString.h
@@ -46,6 +46,34 @@ public:
 		return *this;
 	}
 
+	BitString& operator<<=(std::size_t t)
+	{
+		if (t < 32) {
+			unsigned long u = m_data[0] >> (32 - t);
+			m_data[0] <<= t;
+			m_data[1] <<= t;
+			m_data[1] |= u;
+		} else {
+			m_data[1] = m_data[0] << (t - 32);
+			m_data[0] = 0;
+		}
+		return *this;
+	}
+
+	BitString& operator>>=(std::size_t t)
+	{
+		if (t < 32) {
+			unsigned long u = m_data[1] << (32 - t);
+			m_data[1] >>= t;
+			m_data[0] >>= t;
+			m_data[0] |= u;
+		} else {
+			m_data[0] = m_data[1] >> (t - 32);
+			m_data[1] = 0;
+		}
+		return *this;
+	}
+
 	BitString operator~()
 	{
 		BitString result;
@@ -72,6 +100,21 @@ public:
 	{
 		BitString result(x);
 		result ^= y;
+		return result;
+	}
+
+
+	friend BitString operator <<(const BitString& x, std::size_t t)
+	{
+		BitString result(x);
+		result <<= t;
+		return result;
+	}
+
+	friend BitString operator >>(const BitString& x, std::size_t t)
+	{
+		BitString result(x);
+		result >>= t;
 		return result;
 	}
 
--- dz1.cpp
+++ dz1.cpp
@@ -7,13 +7,14 @@ int main()
 {
 	BitString b;
 	BitString b0(1);
-	BitString b31(0, 1);
+	BitString b32(0, 1);
 	cout << b << endl;
 	cout << b0 << endl;
-	cout << b31 << endl;
+	cout << b32 << endl;
 	cout << ~b << endl;
 	cout << ~b0 << endl;
 	cout << (~b0 & b0) << endl;
-	cout << (b31 | b0) << endl;
+	cout << (b32 | b0) << endl;
+	cout << (b0 << 32) << endl;
 	return 0;
 }
